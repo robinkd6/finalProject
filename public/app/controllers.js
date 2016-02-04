312893f67b4f507bfa1403358691becef4872ab6
jogit82 <jo.c.keller@gmail.com>
2016-02-04 11:16:12 -0800

    morning pull

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,15 +3,21 @@ appCtrls.controller('PersonalityCtrl', ['$scope', '$http', function($scope, $htt
 	$scope.hide = true;
 	$scope.analyze = function(){
 		$scope.big5s = [];
+		$scope.openness = [];
+		$scope.conscientiousness = [];
+		$scope.extraversion = [];
+		$scope.agreeableness = [];
+		$scope.neuroticism = [];
 		var req = {
 		  url: '/api/personality/analyze/'+$scope.summary
 		}
 		$http(req).then(function success(res) {
-			
+			console.log(res.data);
 			if(Array.isArray(res.data)){
 				$scope.hide = false;
 				$scope.hideError = true;
 				var personality = res.data[0].children[0].children;
+				console.log(personality);
 				for (var i = 0; i < personality.length; i ++) {
 					$scope.big5s.push(personality[i]);
 				} 
