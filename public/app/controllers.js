1f579cd0335be72db1a8092c88ab0cf142a0850d
robinkd6 <rkdcruz18@gmail.com>
2016-02-08 02:51:07 -0800

    fixed footer

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,5 +1,5 @@
 var appCtrls = angular.module('AppCtrls', ['app']);
-appCtrls.controller('PersonalityCtrl', ['$scope', '$http', 'UserService', function($scope, $http, UserService){
+appCtrls.controller('PersonalityCtrl', ['$scope', '$http', 'UserService', 'angularSlideables', function($scope, $http, UserService, angularSlideables){
 	$scope.hide = true;
 	$scope.hideAll = true;
 
