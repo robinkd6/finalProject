747e23e537df23449fd2e44a2e0ba8111971db40
jogit82 <jo.c.keller@gmail.com>
2016-02-12 19:55:39 -0800

    done for now

--- routes/personality.js
+++ routes/personality.js
@@ -26,24 +26,24 @@ router.route('/analyze/:summary')
 	});
 });
 
-router.route('/analyze/:message')
-.get(function(req, res) {
-	var document_conversion = watson.document_conversion({
-	username: process.env.TONE_USERNAME,
-	password: process.env.TONE_PASSWORD,
-	version: 'v1'
-});
+// router.route('/analyze/:message')
+// .get(function(req, res) {
+// 	var document_conversion = watson.document_conversion({
+// 	username: process.env.TONE_USERNAME,
+// 	password: process.env.TONE_PASSWORD,
+// 	version: 'v1'
+// });
 
-	var my_message = req.params.message;
+// 	var my_message = req.params.message;
 
-	document_conversion.tone({ text: my_message },
-	function (err, tone) {
-		if(err)
-			res.send(err)
-		else
-			res.send(tone);
-	});
-});
+// 	document_conversion.tone({ text: my_message },
+// 	function (err, tone) {
+// 		if(err)
+// 			res.send(err)
+// 		else
+// 			res.send(tone);
+// 	});
+// });
 
 router.route('/save')
 .post(function(req, res) {
