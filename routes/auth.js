1a1a33124da4adcfccc587bbe7f43d4f56b5dcc1
robinkd6 <rkdcruz18@gmail.com>
2016-09-05 21:38:52 -0700

    sql

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var db = require('../models');
+var passport = require('passport');
+var router = express.Router();
+
+//Authentication
+router.
+
+module.exports = router;
